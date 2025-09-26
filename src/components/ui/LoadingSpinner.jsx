01b8ccaf3e09894985f63a2006ca533ab1bb1945
drzee1205 <drzee12051205@duck.com>
2025-09-26 13:05:53 +0500

    Add files via upload

---
+++ src/components/ui/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function LoadingSpinner() {
+  return (
+    <div className="loading-dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
+
